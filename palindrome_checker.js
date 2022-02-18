bc211a5e9944590c0e204582eab58d0be678eb88
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-18 17:04:20 +0100

    Adding comments to the code

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -3,7 +3,8 @@ function palindrome() {
     // and match checking by using regex and modifier 'g' which perform a global match on a-z letters and 0-9 numbers
     var str = document.getElementById("input").value;
     const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g); 
-    //  return if they are equal and also using join to remove spacing in the words.
+    // check if they are equal and also using join to remove spacing in the words.
+    // If they are equal send output to the html documtent, else send different output to html document.
     if(alphanumericOnly.join('') === alphanumericOnly.reverse().join('')) {
         var palindrome_answer = "It's a palindrome!";
         document.getElementById('output').innerHTML = palindrome_answer;
