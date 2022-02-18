90f17c6e80f8d0f0d8a1eb2cb5d142abcc652d79
Bjørnar Borge <72221999+bjurneiz@users.noreply.github.com>
2022-02-18 15:03:16 +0100

    finished the javascript and form connection. Project done

--- palindrome_checker.js
+++ palindrome_checker.js
@@ -1,11 +1,14 @@
-function palindrome(str) {
+function palindrome() {
     // Adding the parameter str to a new const while we use the methods toLowerCase
     // and match checking by using regex and modifier 'g' which perform a global match on a-z letters and 0-9 numbers
+    var str = document.getElementById("input").value;
     const alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g); 
-        
     //  return if they are equal and also using join to remove spacing in the words.
-    return alphanumericOnly.join('') ===
-        alphanumericOnly.reverse().join('');
+    if(alphanumericOnly.join('') === alphanumericOnly.reverse().join('')) {
+        var palindrome_answer = "It's a palindrome!";
+        document.getElementById('output').innerHTML = palindrome_answer;
+    } else {
+        var palindrome_answer2 = "It's not a palindrome!"
+        document.getElementById('output').innerHTML = palindrome_answer2;
+    }
 }
-
-palindrome("eye");
